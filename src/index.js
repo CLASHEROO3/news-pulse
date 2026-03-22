ccb57b07a75b2505cbdec695fa82da3769eb6a1a
CLASHEROO3 <sagawade112@gmail.com>
2026-03-22 16:51:19 +0530

    Final fix for the title

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// THIS FORCES THE TITLE THE MOMENT THE APP LOADS
+document.title = "NewsPulse | Indian News Aggregator";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />
